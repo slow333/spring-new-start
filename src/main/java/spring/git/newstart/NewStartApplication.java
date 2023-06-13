ae8e19e58d5e0cee5ce539a4fa5ade6f047a887f
slow333 <slow33@gmail.com>
2023-06-13 12:38:00 +0900

    initializing spring boot project

---
+++ src/main/java/spring/git/newstart/NewStartApplication.java
@@ -0,0 +1,13 @@
+package spring.git.newstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/git/newstart/NewStartApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.git.newstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
